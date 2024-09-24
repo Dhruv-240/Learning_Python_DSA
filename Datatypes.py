be9e12054eb1b9c8b6f5dceb4dc1eb9674d14db5
Dhruv-240 <us3l3ss28@gmail.com>
2024-09-25 05:11:56 +0530

    new file:   Datatypes.py 	modified:   First-program.py 	deleted:    fizzbuzz.py

---
+++ Datatypes.py
@@ -0,0 +1,17 @@
+'''
+Type of Datatypes
+1. int - Numerical types
+2. float- Decimal types, Takes more space than int
+3. string- alphabets symbols and numbers
+4. boolean
+5 list
+6. tuple
+7. dictionary
+8 set
+'''
+
+
+print(2+4)
+print((type(2)))
+print(type(2+4)) # gives int
+print(type(2/4))  # gives float
--- First-program.py
+++ First-program.py
@@ -1,25 +1,5 @@
-'''Asked gemini to give me a basic problem staement:
+a = input("what is your name?: ")
+print(a)
 
-  Write a Python program that generates a random number between 1 and 100. The user should then guess the number. If the guess is too high or too low, the program should give a hint. The game should continue until the user guesses the correct number.
-'''
 
-import random
-A = random.randint(1,100)
 
-guesses = 0
-while True:
-    
-    user_input = int(input("Guess The number: "))
-    guesses+=1
-    if user_input>A:
-        print("the random number is smaller than", user_input)
-    elif user_input<A:
-        print("the random number is bigger than", user_input)
-    else:
-        print("Ding! Ding! Ding!, you guessed the number", A, "in",guesses,"guesses")
-        play_again= input("Would you like to play again?(y/n)").lower()
-        if play_again=="y":
-            A = random.randint(1,100)
-            guesses = 0
-        else:    
-         break
